6f5cc6d341dcdfd74a6c072cb90bf87b9b0407cb
santlal <nitinrohilla@virtualemployee.com>
2018-03-06 19:49:41 +0530

    Guaranter Form

---
+++ AIMFinServClientApp/src/app/layout/masters/loanguarantor/loanguarantor.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { LoanGuarantorRoutingModule } from './loanguarantor-routing.module';
+import { LoanGuarantorComponent } from './loanguarantor.component';
+import { PageHeaderModule } from '../../../shared';
+import {MaterialModule} from '../../../shared/app.material';
+import { FormsModule } from '@angular/forms';
+@NgModule({
+    imports: [CommonModule, LoanGuarantorRoutingModule, PageHeaderModule, GridModule, MaterialModule, FormsModule],
+    declarations: [LoanGuarantorComponent]
+})
+export class LoanGuarantorModule { }
