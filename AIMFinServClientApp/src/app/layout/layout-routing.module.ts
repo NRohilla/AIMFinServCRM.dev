185a73b2a052848bdcf8e91b627da76c7c2c9f6a
NRohilla <NitinRohilla@virtualemployee.com>
2018-03-07 12:46:18 +0530

    Commented unused modules from routing of layout

--- AIMFinServClientApp/src/app/layout/layout-routing.module.ts
+++ AIMFinServClientApp/src/app/layout/layout-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
         children: [
             { path: '', redirectTo: 'dashboard' },
             { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-            { path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+            
             { path: 'clients', loadChildren: './clients/clients.module#ClientsModule' },
             { path: 'loanapplications', loadChildren: './LoanApplications/loanapplications.module#LoanapplicationsModule' },
 
@@ -29,12 +29,13 @@ const routes: Routes = [
             { path: 'master-loanguarantor', loadChildren: './masters/loanguarantor/loanguarantor.module#LoanGuarantorModule' },
             { path: 'master-loanmaster', loadChildren: './masters/loanmaster/loanmaster.module#LoanMasterModule' },
 
-            { path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
-            { path: 'forms', loadChildren: './form/form.module#FormModule' },
-            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
-            { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
             { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-            { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
+            { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
+            //{ path: 'charts', loadChildren: './charts/charts.module#ChartsModule' },
+            //{ path: 'tables', loadChildren: './tables/tables.module#TablesModule' },
+            //{ path: 'forms', loadChildren: './form/form.module#FormModule' },
+            //{ path: 'grid', loadChildren: './grid/grid.module#GridModule' },
+            //{ path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
     }
 ];
