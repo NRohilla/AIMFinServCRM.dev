19ddc645f87619c4ac5109730cf88c8602382cb0
Neha Bambah <nitinrohilla@virtualemployee.com>
2018-03-07 15:43:30 +0530

    Created forms for the loanapplication tabs

--- AIMFinServClientApp/src/app/layout/LoanApplications/components/app.applicant.personaldetails.ts
+++ AIMFinServClientApp/src/app/layout/LoanApplications/components/app.applicant.personaldetails.ts
@@ -1,7 +1,7 @@
-﻿import { Component, Injectable, ViewChild, OnInit, ElementRef, Inject  } from '@angular/core';
+import { Component, Injectable, ViewChild, OnInit, ElementRef, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { routerTransition } from '../../../router.animations';
-import { Form, FormControl, FormBuilder, Validators  } from '@angular/forms';
+import { Form, FormControl, FormBuilder, Validators } from '@angular/forms';
 import { LocalStorageService } from 'angular-2-local-storage';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -14,8 +14,33 @@ import { Http, Headers, RequestOptions, Response } from '@angular/http';
     providers: []
 })
 export class ApplicantPersonalDetailsComponent implements OnInit {
+
+    public _ApplicantPersonalDetails= {
+       ApplicantID : '',
+       FirstName: '',
+       MiddleName: '',
+       LastName: '',
+       Gender: '',
+       DateOfBirth: '',
+       MaritalStatus: '',
+       NoOfDependents: '',
+       NZResidents: '',
+       CountryOfBirth: '',
+       ApplicantTypeID: '',
+       EmailID: '',
+       MobileNo: '',
+       HomePhoneNo: '',
+       WorkPhoneNo: '',
+    };
+
+
     constructor(public router: Router, private _LocalStorageService: LocalStorageService) { }
     ngOnInit() {
+    }
+
+    testpersonal() {
+        debugger;
+        return true;
     }
 }
 
