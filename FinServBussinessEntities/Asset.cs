5d3bb20183ba3fe02583dd0c9f4953a8358f13e5
Neha Bambah <nitinrohilla@virtualemployee.com>
2018-03-13 13:58:28 +0530

    files added

---
+++ FinServBussinessEntities/Asset.cs
@@ -0,0 +1,27 @@
+﻿using FinServDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinServBussinessEntities
+{
+    public class Asset
+    {
+        public long AutoID { get; set; }
+        public System.Guid AssetID { get; set; }
+        public Nullable<System.Guid> AssetTypeID { get; set; }
+        public string Description { get; set; }
+        public Nullable<decimal> NetValue { get; set; }
+        public string Ownership { get; set; }
+        public System.Guid ApplicantID { get; set; }
+        public Nullable<System.Guid> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public Nullable<System.Guid> ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+
+        public Applicants _ApplicationID { get; set; }
+        public AssetsTypeMaster _AssetTypeID { get; set; }
+    }
+}
---
+++ FinServBussinessEntities/Liability.cs
@@ -0,0 +1,27 @@
+﻿using FinServDataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinServBussinessEntities
+{
+    public class Liability
+    {
+        public long AutoID { get; set; }
+        public System.Guid LiabilityID { get; set; }
+        public Nullable<System.Guid> LiabilityTypeID { get; set; }
+        public string Description { get; set; }
+        public Nullable<decimal> NetValue { get; set; }
+        public string Ownership { get; set; }
+        public System.Guid ApplicantID { get; set; }
+        public Nullable<System.Guid> CreatedBy { get; set; }
+        public Nullable<System.DateTime> CreatedOn { get; set; }
+        public Nullable<System.Guid> ModifiedBy { get; set; }
+        public Nullable<System.DateTime> ModifiedOn { get; set; }
+
+        public Applicants _ApplicationID { get; set; }
+        public LiabilityTypeMaster _LiabilityID { get; set; }
+    }
+}
