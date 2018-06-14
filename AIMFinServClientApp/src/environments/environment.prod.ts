739bfa98d6b2dd0f2807de12e5ad0118fb420c52
Shrangali Shukla <Shrangali Shukla@VE140418C>
2018-06-14 14:27:35 +0530

    Add base url in environment prod

--- AIMFinServClientApp/src/environments/environment.prod.ts
+++ AIMFinServClientApp/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     testing: false,
     //for local connection to the API service
     baseAPIUrl: "http://182.75.120.10:9393/AIMFinServAPI/API/",
-    baseApplicationURL: "http://182.75.120.10:9393/AimFinServe_Admin/",
-    baseClientAppURL: "http://182.75.120.10:9393/AimFinServe_Client/"
+    baseApplicationURL: "http://www.loaner.co.nz/Admin/",
+    baseClientAppURL: "http://www.loaner.co.nz/Clients/"
 };
